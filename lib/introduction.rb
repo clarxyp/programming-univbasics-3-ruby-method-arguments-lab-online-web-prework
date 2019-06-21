5c97217d3064e2e41fdaff623281a328ab2c7804
Clara Yip <clara.yip1@gmail.com>
2019-06-21 21:30:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,3 +7,11 @@ puts introduction(name)
 
 name = "Dan"
 puts introduction(name)
+
+language = "Ember.js"
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #(language}.\n"
+end
+
+puts introduction_with_language(name, language)
