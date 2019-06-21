707ec32fdb7243b31254ede04b0c2c1403a64b17
Clara Yip <clara.yip1@gmail.com>
2019-06-21 21:37:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,3 +15,11 @@ def introduction_with_language(name, language)
 end
 
 puts introduction_with_language(name, language)
+
+language = 
+
+def introduction_with_language(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+puts introduction_with_language(name, language)
